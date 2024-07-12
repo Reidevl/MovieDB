4f30ceb4af596bc537093b06bf06be9b535f9735
Reiwil Lugo <reiwillugo@gmail.com>
2024-07-11 20:56:58 -0400

    Initial Commit

---
+++ MoviesDB/MoviesDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDBApp.swift
+//  MoviesDB
+//
+//  Created by Reiwil Lugo on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
